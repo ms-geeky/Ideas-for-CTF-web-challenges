b6e6eec329760ada1dff5d0fa256ccf9fe8e1e69
Swetha Sridevi <swethasridevi@protonmail.com>
2021-09-06 17:43:03 +0200

    now working client cert with firefox

--- docker-cert-and-git-poc/generatekeypairandcsr.sh
+++ docker-cert-and-git-poc/generatekeypairandcsr.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 
-# openssl req -newkey rsa:2048 -x509 -nodes -keyout server.key -new -out server.crt -subj /CN=localhost -sha256 -days 3650
 
 # generate ca key and cert
-openssl req -newkey rsa:2048 -nodes -keyform PEM -keyout ca.key -x509 -days 3650 -outform PEM -subj /CN=CA -out ca.crt 
+openssl req -config ca_openssl.conf -x509 -new -nodes -newkey rsa -keyout ca.key -days 3650 -out ca.crt 
+
+# create databases files for ca
+touch serial.txt
+touch index.txt
+touch rand
 
 # create server key
 openssl genrsa -out server.key 2048 
-openssl req -new -key server.key -subj /O=BIT -out server.csr
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -set_serial 100 -days 365 -outform PEM -out server.crt 
+openssl req -new -key server.key -subj /CN=localhost -out server.csr
+# let server cert be signed by CA
+openssl ca -batch -config ca_openssl.conf -keyfile ca.key -cert ca.crt -policy signing_policy -out server.crt -infiles server.csr 
 
 # create client cert and p12 store; only O=BIT is important!
 openssl genrsa -out client.key 2048 
 openssl req -new -key client.key -subj /CN=Haxxorman/C=US/O=BIT -out client.csr
-openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -set_serial 100 -days 365 -outform PEM -out client.crt 
+# let client cert be signed by CA
+openssl ca -batch -config ca_openssl.conf -keyfile ca.key -cert ca.crt -policy signing_policy -out client.crt -infiles client.csr 
+# export client key and cert to p12 store
 openssl pkcs12 -export -inkey client.key -in client.crt -out client.p12 -passin pass:test123 -passout pass:test123
